e1a959bdc0e2d662c58563dd6c147c671067d1ee
Mark Madhukar <markmadhukar03@gmail.com>
2021-05-09 13:18:38 +0530

    commit - 3

--- quote-parser.py
+++ quote-parser.py
@@ -3,20 +3,22 @@ from bs4 import BeautifulSoup as soup
 import json
 
 request = rq.get("https://pakwired.com/100-best-quotes-time/")
-parser = soup(request.text)
+parser = soup(request.text, 'html.parser')
 
 quotes_raw = parser.find_all("blockquote")
 quotes = []
+fin = {}
 
 for quote in quotes_raw:
     quotes.append(
-        {"quote": quote.find("p").text.split(".")[1],
+        {"quote": quote.find("p").text,
          "author": quote.find("cite").text}
         )
 
+fin["data"] = quotes
 
 
 with open('quotes.json', 'w') as fp:
-    quotes = json.dumps(quotes)
-    json.dump(quotes, fp)
+    quotes = json.dumps(fin)
+    json.dump(fin, fp)
     
