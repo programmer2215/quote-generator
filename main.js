b348d1e7b8be8cdc7692d3a3096316b5166a767f
Mark Madhukar <markmadhukar03@gmail.com>
2022-01-13 20:25:46 +0530

    aesthetic changes & minor bug fixes

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ async function get_data(){
     var index = Math.floor(Math.random() * 100 );
     var quote_json = response[index];
     const quote = quote_json.text;
-    const author = quote_json.author;
-    if (author == null){
+    var author = quote_json.author;
+    if (author == undefined){
         author = "Unknown";
     };
     document.getElementById('quote-box').innerHTML =  `\u201c${quote}\u201d <br> <hr> <b><i> \u2013${author} </i></b>`
@@ -22,14 +22,6 @@ async function get_data(){
     
 }
 
-    
+document.addEventListener("DOMContentLoaded", get_data);
 
-
-
-document.addEventListener("DOMContentLoaded", function(){
-   get_data();
-
-    
-    //var quote = data_json.data[index].quote;
-    
-});
+document.addEventListener("click", get_data);
